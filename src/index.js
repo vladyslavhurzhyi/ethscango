3914b724e04ac89a44ff93dce7394c03903ce547
Vladyslav Hurzhyi <soulline0@gmail.com>
2023-02-02 16:26:46 +0100

    getMarketCap

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import { BrowserRouter } from 'react-router-dom';
 import App from 'components/App';
 import './index.css';
 
+import { getMarketCapEth } from 'API/getDataMarketCap';
+
+getMarketCapEth();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
