54c941978926cc145965ad1960cce1bea3c09835
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2023-02-03 17:19:22 +0200

    Updates app bar with navigation

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,10 +1,39 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Navigation } from '../Navigation/Navigation';
+import { BiMenu } from 'react-icons/bi';
+import { FaEthereum } from 'react-icons/fa';
 
 export const AppBar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const navVisibilityClassName = navbarOpen ? 'flex' : 'hidden';
   return (
-    <header>
-      <p>EthScanGo</p>
-      <Navigation />
+    <header className="relative flex flex-wrap items-center justify-between px-2 py-2 dark:bg-slate-800 mb-3 shadow-md">
+      <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+        <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+          <Link
+            to="/"
+            className="flex items-center text-lg leading-relaxed py-2 mr-4 whitespace-nowrap dark:text-white"
+          >
+            <FaEthereum className="mr-1 text-blue-500" size={22} />
+            EthScanGo
+          </Link>
+
+          <button
+            className="dark:text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+            type="button"
+            onClick={() => setNavbarOpen(!navbarOpen)}
+          >
+            <BiMenu size={32} />
+          </button>
+        </div>
+
+        <nav
+          className={`lg:flex flex-grow items-center ${navVisibilityClassName}`}
+        >
+          <Navigation />
+        </nav>
+      </div>
     </header>
   );
 };
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,26 @@
 import { NavLink } from 'react-router-dom';
 
-export const Navigation = () => {
-  return (
-    <nav>
-      <NavLink to="/">HOME</NavLink>
-      <NavLink to="/address">ADDRESS</NavLink>
-      <NavLink to="/transaction">TRANSACTION</NavLink>
-    </nav>
-  );
+const navItems = [
+  { href: '/', text: 'Home' },
+  { href: 'address', text: 'Address' },
+  { href: 'transaction', text: 'Transaction' },
+];
+
+const setLinkClassName = ({ isActive }) => {
+  const baseLinkClass = 'px-1 lg:px-3 py-2 flex items-center text-sm';
+  return isActive
+    ? `${baseLinkClass} text-blue-500`
+    : `${baseLinkClass} dark:text-white hover:opacity-75`;
 };
+
+export const Navigation = () => (
+  <ul className="w-full lg:w-auto flex flex-col lg:flex-row list-none lg:ml-auto">
+    {navItems.map(({ href, text }) => (
+      <li key={href} className="nav-item">
+        <NavLink to={href} className={setLinkClassName}>
+          {text}
+        </NavLink>
+      </li>
+    ))}
+  </ul>
+);
