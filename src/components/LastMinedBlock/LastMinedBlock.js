2152f61e099d0a753f588ae7fb1b57cec4c76b2e
IreneCreadora <nargiza.dancer@gmail.com>
2023-02-07 16:22:31 +0000

    add styles for last mined block

--- src/components/LastMinedBlock/LastMinedBlock.js
+++ src/components/LastMinedBlock/LastMinedBlock.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import { getLastMinedBlock } from '../../API/getData';
 
+import { SiCodesandbox } from 'react-icons/si';
+
 export const LastMinedBlock = () => {
   const [data, setData] = useState([]);
 
@@ -14,11 +16,12 @@ export const LastMinedBlock = () => {
   }, []);
 
   return (
-    <div>
-      <hr />
-      <p>The most recently mined block</p>
-      <b>{data}</b>
-      <hr />
+    <div className="flex items-center  m-1 p-4 w-1/4 border border-solid border-indigo-500/75 rounded">
+      <SiCodesandbox className="mr-2 text-purple-600" size={48} />
+      <div className="flex justify-between w-full">
+        <h3 className="w-40 text-lg">The most recently mined block</h3>
+        <p className="text-lg font-bold">{data}</p>
+      </div>
     </div>
   );
 };
