445fc1e8bf74a5f112aa1150697578436547b6ed
Vladyslav Hurzhyi <soulline0@gmail.com>
2023-03-03 12:20:19 +0100

    add ethprice + MC

---
+++ src/components/MarketCap/MarketCap.js
@@ -0,0 +1,30 @@
+import { getMarketCapEth } from 'API/getDataMarketCap';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { RiEarthFill } from 'react-icons/ri';
+
+export const MarketCap = () => {
+  const [marketCap, setMarketCap] = useState(null);
+
+  useEffect(() => {
+    async function getMarketCapData() {
+      const data = await getMarketCapEth();
+      setMarketCap(data);
+    }
+    getMarketCapData();
+  }, []);
+
+  return (
+    <>
+      {marketCap ? (
+        <div className="flex items-center  m-1 p-4 w-1/4 border border-solid border-indigo-500/75 rounded">
+          <RiEarthFill className="mr-2 text-purple-600" size={48} />
+          <div className="flex justify-between w-full">
+            <h3 className="w-40 text-lg">MARKET CAP ETH</h3>
+            <p className="text-lg font-bold">{marketCap}</p>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
+};
